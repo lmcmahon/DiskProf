3ca21c91e05d3fb6626a7dad450cb40de7dc1d8c
Liam McMahon <lmmcmahon9@gmail.com>
2012-03-15 23:30:15 -0500

    now include non-directory children, and made make-file-rec look better

--- src/DiskProf/core.clj
+++ src/DiskProf/core.clj
@@ -1,20 +1,16 @@
 (ns DiskProf.core
   ;(:use 'seesaw.core)
   )
-(:use 'seesaw.core)
+(use 'seesaw.core)
 (import 'java.io.File)
+;(import 'javax.swing.JPanel)
 
 (defrecord file-rec [file size children])
 
 (defn make-file-rec [file]
   "Takes a java.io.File and returns a file-rec"
-  (let [children (seq (.listFiles file))
-	child-dirs (filter #(.isDirectory %) children)
-	child-files (filter #(.isFile %) children)
-	child-file-recs (map make-file-rec child-dirs)]
+  (let [child-recs (map make-file-rec (seq (.listFiles file)))]
     (file-rec. file
-	       (reduce + (.length file)
-		       (concat (map #(.length %) child-files)
-			     (map :size child-file-recs)))
-	       child-dirs)))
+	       (reduce + (.length file) (map :size child-recs))
+	       child-recs)))
 
