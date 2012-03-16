aa1550eb9059929d44e03cd3c2d781dc80e5d6d9
Liam McMahon <lmmcmahon9@gmail.com>
2012-03-15 20:07:13 -0500

    make-file-rec works

--- src/DiskProf/core.clj
+++ src/DiskProf/core.clj
@@ -1,1 +1,20 @@
-(ns DiskProf.core)
+(ns DiskProf.core
+  ;(:use 'seesaw.core)
+  )
+(:use 'seesaw.core)
+(import 'java.io.File)
+
+(defrecord file-rec [file size children])
+
+(defn make-file-rec [file]
+  "Takes a java.io.File and returns a file-rec"
+  (let [children (seq (.listFiles file))
+	child-dirs (filter #(.isDirectory %) children)
+	child-files (filter #(.isFile %) children)
+	child-file-recs (map make-file-rec child-dirs)]
+    (file-rec. file
+	       (reduce + (.length file)
+		       (concat (map #(.length %) child-files)
+			     (map :size child-file-recs)))
+	       child-dirs)))
+
